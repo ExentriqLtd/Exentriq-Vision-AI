92338c6d0f1c7b30853b546c77f806ad8ce99108
Larens94 <fabrizio.corpora@gmail.com>
2023-10-03 14:54:01 +0200

    implement new page chooseFromFolder with google drive picker

---
+++ frontend/src/pages/chooseFromFolder.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+
+import type { NextPage } from "next";
+import useDrivePicker from 'react-google-drive-picker'
+
+const ChooseFromFolder: NextPage = () => {
+  const [fileDrive, setFileDrive] = useState({});
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [openPicker, authResponse] = useDrivePicker();
+
+
+  const handleOpenPicker = () => {
+    openPicker({
+      clientId: "841515951225-t0foo58s6s8jt6oafvgi0e9dcfn3e22i.apps.googleusercontent.com",
+      developerKey: "AIzaSyApUTcsAu8uKlekoLbjT5L8GroNFYDkhuw",
+      viewId: "DOCS",
+      // token: token, // pass oauth token in case you already have one
+      showUploadView: true,
+      showUploadFolders: true,
+      supportDrives: true,
+      multiselect: true,
+      // customViews: customViewsArray, // custom view
+      callbackFunction: (data) => {
+        if (data.action === 'cancel') {
+          console.log('User clicked cancel/close button')
+        }
+        setFileDrive(data?.docs)
+        console.log('callbackFunction::', data)
+      },
+    })
+  }
+
+  return (
+    <>
+      <div className="mb-3 flex-1">
+        <div className="max-w-2xl text-left">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Choose from folder</h2>
+          <p className="mt-2 text-3xl leading-8 text-gray-600">Data Sources</p>
+        </div>
+        <div
+          className="
+          block
+          w-full
+          rounded-md
+          bg-white
+          px-3.5
+          py-2.5
+          my-4
+          text-left
+          text-sm
+          text-dark
+          shadow-md
+          flex
+          items-center
+          justify-between
+          hover:bg-white
+          focus-visible:outline
+          focus-visible:outline-2
+          focus-visible:outline-offset-2 
+          focus-visible:outline-indigo-600">
+          <div className="flex items-center">
+            <img src={'./drive-folder.png'} className="w-10" alt="Google Drive Folder" />
+            <p className="px-2">Google Drive</p>
+          </div>
+          <button className="underline" onClick={() => handleOpenPicker()}>Open folder</button>
+        </div>
+        <div
+          className="
+          block
+          w-full
+          rounded-md
+          bg-white
+          px-3.5
+          py-2.5
+          my-4
+          text-left
+          text-sm
+          text-dark
+          shadow-md
+          flex
+          items-center
+          justify-between
+          hover:bg-white
+          focus-visible:outline
+          focus-visible:outline-2
+          focus-visible:outline-offset-2 
+          focus-visible:outline-indigo-600">
+          <div className="flex items-center">
+            <img src={'./exentriq-folder.png'} className="w-10" alt="Google Drive Folder" />
+            <p className="px-2">Exentriq Document Manager Folder</p>
+          </div>
+          <button className="underline" onClick={() => {}}>Open folder</button>
+        </div>
+      </div>
+      <progress value={uploadProgress} max="100"></progress>
+    </>
+  );
+};
+export default ChooseFromFolder;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -14,8 +14,7 @@ const LandingPage: NextPage = () => {
   const [uploadProgress, setUploadProgress] = useState(0);
   const [isLoadingConversation, setIsLoadingConversation] = useState(false);
   const router = useRouter();
-
-  const handleUpload = (event) => {
+  const handleUpload = (event: any) => {
     setFile(event.target.files[0]);
     event.preventDefault();
     const url = backendUrl + 'api/upload'; //non funziona ma il concetto è questo
@@ -26,7 +25,7 @@ const LandingPage: NextPage = () => {
       headers: {
         'content-type': 'multipart/form-data',
       },
-      onUploadProgress: function(progressEvent) {
+      onUploadProgress: function (progressEvent: any) {
         console.log('progress', progressEvent);
         const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
         setUploadProgress(percentCompleted);
@@ -56,24 +55,44 @@ const LandingPage: NextPage = () => {
       })
       .catch(() => console.log("error creating conversation "));
 
-}
+  }
 
   return (
     <>
       {/* <TitleAndDropdown /> */}
       <div className="mb-3 w-96">
-          <label
-            htmlFor="formFile"
-            className="mb-2 inline-block text-neutral-700 dark:text-neutral-200"
-          >
+        <label
+          htmlFor="formFile"
+          className="mb-2 inline-block text-neutral-700 dark:text-neutral-200"
+        >
           Upload
-          </label>
-          <input
-            className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
-            type="file"
-           id="formFile"
-           onChange={handleUpload}
-          />
+        </label>
+        <input
+          className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
+          type="file"
+          id="formFile"
+          onChange={handleUpload}
+        />
+        <button
+          onClick={() => router.push(`/chooseFromFolder/`)}
+          className="
+          block 
+          w-full 
+          rounded-sm 
+          bg-primary-ex 
+          px-3.5 
+          py-2.5 
+          text-center 
+          text-sm 
+          text-white 
+          shadow-md 
+          hover:bg-primary-ex 
+          focus-visible:outline 
+          focus-visible:outline-2 
+          focus-visible:outline-offset-2 
+          focus-visible:outline-indigo-600">
+          Choose from folder
+        </button>
       </div>
       <progress value={uploadProgress} max="100"></progress>
 
