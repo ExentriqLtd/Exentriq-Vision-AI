69e33c1cedc77483678a81331c4b5de11b3edf6e
Larens94 <fabrizio.corpora@gmail.com>
2023-10-04 16:32:49 +0200

    implement collection component UI

--- frontend/src/pages/section/leftSidebar/CollectionsList.tsx
+++ frontend/src/pages/section/leftSidebar/CollectionsList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { backendClient } from "~/api/backend";
 import { SecCollections } from "~/types/collections";
+import Collection from "./collection";
 
 const CollectionList: React.FC = () => {
   const [availableCollections, setAvailableCollections] = useState<SecCollections[]>([]);
@@ -31,15 +32,14 @@ const CollectionList: React.FC = () => {
            sm:text-sm sm:leading-6"
         placeholder="Search..." />
       <p className="mt-6 text-gray-400 text-sm">There are no conversation yet you can start one <span className="color-primary-ex text-semibold underline">here</span> </p>
-      {availableCollections.map((collection) => {
-        return (
-          <div
-            key={`collection-${collection.name}`}
-          >
-            <div>{collection.name}</div>
-          </div>
-        );
-      })}
+      <ul className="w-full my-5">
+        {availableCollections.map((collection, index) => {
+          console.log('collection...',collection)
+          return (
+            <Collection index={index} name={collection?.name} created_at={collection?.created_at}/>
+          );
+        })}
+      </ul>
       <div className="absolute bottom-10 right-10 shadow-md bg-primary-ex w-10 h-10 rounded-full flex justify-center items-center">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 text-white h-5">
           <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
