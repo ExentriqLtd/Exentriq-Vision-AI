b61ee8dca264d43ed9ab841e83804727e7479d0c
Larens94 <fabrizio.corpora@gmail.com>
2023-10-16 13:49:38 +0200

    Add error from upload if file is not pdf

--- frontend/src/components/basics/DragAndDrop.tsx
+++ frontend/src/components/basics/DragAndDrop.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import React, { Fragment, useRef, useState } from "react";
 
 type Props = {
   onUpload: (files: File[]) => void;
@@ -6,6 +7,8 @@ type Props = {
 
 const DragAndDrop = ({ onUpload }: Props) => {
   const [isDragActive, setIsDragActive] = useState<boolean>(false);
+  const [open, setOpen] = useState(false)
+  const cancelButtonRef = useRef(null)
 
   const handleDragEnter = () => {
     setIsDragActive(true);
@@ -19,28 +22,88 @@ const DragAndDrop = ({ onUpload }: Props) => {
     e.preventDefault();
     setIsDragActive(false);
     const files = Array.from(e.dataTransfer.files);
-    onUpload(files);
+    files?.map((file) => {
+      if (file?.type !== 'application/pdf') return setOpen(true);
+      onUpload([file]);
+    })
   };
 
   return (
-    <div
-      className={`flex justify-center items-center w-2/3 h-48 border-2 border-dashed rounded-lg p-5
+    <>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                  <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div className="sm:flex sm:items-start">
+                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                          Upload error
+                        </Dialog.Title>
+                        <div className="mt-2">
+                          <p className="text-sm text-gray-500">
+                            The file extension must be PDF, other formats are not accepted
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button
+                      type="button"
+                      className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                      onClick={() => setOpen(false)}
+                      ref={cancelButtonRef}
+                    >
+                      Ok
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
+      <div
+        className={`flex justify-center items-center w-2/3 h-48 border-2 border-dashed rounded-lg p-5
         ${isDragActive ? "bg-sky-50 border-sky-400" : "border-gray-300"}`}
-      onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
-      onDragOver={(e) => e.preventDefault()}
-      onDrop={handleDrop}
-    >
-      <p
-        className={`text-sm ${
-          isDragActive ? "text-sky-800" : "text-gray-400"
-        }  `}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={handleDrop}
       >
-        {isDragActive
-          ? "Leave Your File Here"
-          : "Drag and drop your files here"}
-      </p>
-    </div>
+        <p
+          className={`text-sm ${isDragActive ? "text-sky-800" : "text-gray-400"
+            }  `}
+        >
+          {isDragActive
+            ? "Leave Your File Here"
+            : "Drag and drop your files here"}
+        </p>
+      </div>
+    </>
   );
 };
 
