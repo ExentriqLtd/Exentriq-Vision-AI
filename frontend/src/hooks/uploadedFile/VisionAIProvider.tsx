c0e461ae30f9f47d3304042308b28abd3fa60ce7
Larens94 <fabrizio.corpora@gmail.com>
2023-11-08 13:39:11 +0100

    add interface

--- frontend/src/hooks/uploadedFile/VisionAIProvider.tsx
+++ frontend/src/hooks/uploadedFile/VisionAIProvider.tsx
@@ -2,8 +2,11 @@ import React, { useReducer } from 'react';
 import { VisionAIContext } from './VisionAIContext';
 import { initialState, reducer } from './reducer';
 
+interface ProviderProps {
+  (prevState: any, action: any): any
+}
 export const VisionAIProvider = ({ children }: any) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer<ProviderProps>(reducer, initialState);
 
 
   return (
