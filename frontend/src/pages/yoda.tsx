8fc1b0060d6e8938ef8a641af0b40b2fd1417392
fvenuto <fvenuto87@gmail.com>
2023-11-20 12:27:14 +0100

    added sessionToken and Production Yoda Link

--- frontend/src/pages/yoda.tsx
+++ frontend/src/pages/yoda.tsx
@@ -7,11 +7,12 @@ const Yoda: NextPage = () => {
     const router = useRouter();
 
     const { sessionToken } = router.query;
-    console.log("QUI", sessionToken);
+    const formattedSessionToken = Array.isArray(sessionToken) ? sessionToken[0] : sessionToken;
+
     return (
         <div className="mx-6 w-4/5">
             <div className="w-full h-full">
-                <iframe width={'100%'} height={'100%'} src="https://talk-stage.exentriq.com/chat/demo?sessionToken=iKOfGbDHPkCQvgtMbQBthsuqGEKclDoOqBxqGcFRsiMbJKZZmAVsCipviOWpQiBo"></iframe>
+            <iframe width={'100%'} height={'100%'} src={`https://talk-stage.exentriq.com/chat/yoda?sessionToken=${formattedSessionToken ?? ''}`}></iframe>
             </div>
         </div>
     );
